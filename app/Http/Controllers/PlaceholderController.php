fd6ab5170249db3cc0c7fac2885d7f9a4dab0537
k4h3ny4 <kahenyaa@gmail.com>
2024-08-26 16:37:05 +0300

    posts are loading

--- app/Http/Controllers/PlaceholderController.php
+++ app/Http/Controllers/PlaceholderController.php
@@ -4,28 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class PlaceholderController extends Controller
 {
     /**
      * Display a listing of the posts.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\View\View
      */
     public function index()
     {
         // Make a GET request to the JSONPlaceholder API
         $response = Http::get('https://jsonplaceholder.typicode.com/posts');
 
+        // Log the response to check if it's being fetched correctly
+        Log::info('API Response:', ['response' => $response->json()]);
+
         // Check if the request was successful
         if ($response->successful()) {
-            // Return the JSON response from the API
-            return response()->json($response->json());
+            // Pass the posts data to the view
+            $posts = $response->json();
+            return view('index', ['posts' => $posts]);
         }
 
-        // Handle errors
-        return response()->json([
-            'error' => 'Unable to fetch posts'
-        ], $response->status());
+        // Handle errors (optional: you can also return a different view for errors)
+        return view('index', ['posts' => []]);
     }
 }
